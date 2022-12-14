6f2d80962f1c79495c00d78fbf961acb8252368d
Charles Iliya Krempeaux <iamreiver@gmail.com>
2022-12-14 03:33:55 -0800

    initial commits

---
+++ fileinfo.go
@@ -0,0 +1,39 @@
+package strfs
+
+import (
+	"io/fs"
+	"time"
+)
+
+type internalFileInfo struct {
+	mode fs.FileMode
+	modtime time.Time
+	name string
+	size int64
+}
+
+var _ fs.FileInfo = &internalFileInfo{}
+
+func (receiver internalFileInfo) IsDir() bool {
+	return 0 != (receiver.mode & fs.ModeDir)
+}
+
+func (receiver internalFileInfo) Mode() fs.FileMode {
+	return receiver.mode
+}
+
+func (receiver internalFileInfo) ModTime() time.Time {
+	return receiver.modtime
+}
+
+func (receiver internalFileInfo) Name() string {
+	return receiver.name
+}
+
+func (receiver internalFileInfo) Size() int64 {
+	return receiver.size
+}
+
+func (receiver internalFileInfo) Sys() any {
+	return nil
+}
